745c13b8c1c692dbc793f3858b1ecbdc65435cc6
hvostjuk <hvostjuk@gmail.com>
2020-11-08 15:49:37 +0300

    add button play audio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-let play = document.querySelector('.left-col__player-play');
+'use strict';
+let playAudio = function () {
+    let btnPlay = document.querySelector('.left-col__player-play');
+    let count = 0;
+    btnPlay.addEventListener('click',(e) => {
+        if (count === 0) {
+            btnPlay.src = 'img/last.png'; //меняю кнопку на pause, но у меня ее нету пока, добавлю потом ;]
+            count ++;
+        } else if (count === 1) {
+            btnPlay.src = 'img/play.png';
+            count --;
+        }
+    });
+};
+
+playAudio();
