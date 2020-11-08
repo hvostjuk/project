21a30455ad1a281b0108dd216ff5293f7583a04e
hvostjuk <hvostjuk@gmail.com>
2020-11-08 16:57:18 +0300

    add btn ;)

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@
 let playAudio = function () {
     let btnPlay = document.querySelector('.left-col__player-play');
     let count = 0;
+    let audio = new Audio('aud/1.mp3')
     btnPlay.addEventListener('click',(e) => {
         if (count === 0) {
             btnPlay.src = 'img/last.png'; //меняю кнопку на pause, но у меня ее нету пока, добавлю потом ;]
             count ++;
+            audio.play();
         } else if (count === 1) {
             btnPlay.src = 'img/play.png';
             count --;
+            audio.pause();
         }
     });
 };
-
 playAudio();
